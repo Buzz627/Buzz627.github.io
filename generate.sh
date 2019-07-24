6b077c2b9ebc230b962f8e118fdf2a8acac04a8c
Buzz <Buzz627@gmail.com>
2019-07-24 13:26:45 -0400

    added sed command for stack overflow

--- generate.sh
+++ generate.sh
@@ -6,3 +6,6 @@ hackmyresume build resume/resume.json to testResumes/myResume.html -t node_modul
 hackmyresume build resume/resume.json to testResumes/onePageResume.pdf -t node_modules/jsonresume-theme-onepage-wide/
 rm testResumes/*.css
 rm testResumes/myResume.json
+sed -i "s/icon-lg icon-stackoverflow/fab fa-stack-overflow/" testResumes/myResume.html
+sed -i '/<\/head>/i/<link rel="stylesheet" href="https:\/\/use.fontawesome.com\/releases\/v5.8.1\/css\/all.css">/' testResumes/myResume.html
+
